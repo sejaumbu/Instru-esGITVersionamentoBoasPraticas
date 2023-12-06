feec5cf13092b821d1df6ba5bbeec23b8fb3eb42
ichcamile <84331711+ichcamile@users.noreply.github.com>
2023-12-06 00:39:20 -0300

    Add files via upload

---
+++ ItensSuperior.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function ItensSuperior(props) {
+    return (
+        <div>
+            <div className="itensFixo">
+            </div>
+            <h4 className='reviews'>All Reviews ({props.reviews})</h4>
+        </div>
+    )
+}
+
+
+export default ItensSuperior
---
+++ Portfolio.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Fotos from './Publicacoes/Fotos'
+
+function Portfolio() {
+  return (
+
+    <div>
+    <div className='pagePortfolio'>
+        <div className='ContainerImages'>
+        <Fotos
+        foto= "https://i.pinimg.com/564x/72/5a/5e/725a5ed95921c4fef604a032853df10d.jpg"
+        />
+         <Fotos
+        foto='https://i.pinimg.com/564x/6d/34/a3/6d34a35898bfe9de46ca18e066b5a344.jpg'
+        />
+         <Fotos
+        foto='https://cdn.neighbourly.co.nz/images/cache/message_max_size/message_images/654f31061c61f8.64325519.jpeg?170410'
+        />
+        <Fotos
+        foto='https://www.mooresnursery.com/wp-content/uploads/2021/03/Landscaping-column2-1000px.jpg'
+        />
+        <Fotos
+        foto='https://i.pinimg.com/564x/55/0a/59/550a59d10047ce8e306c332b77a7fa70.jpg'
+        />
+        <Fotos
+        foto='https://www.plantasonya.com.br/wp-content/img/Buxinho-Buxus-sempervirens.jpg'
+        />
+        <Fotos
+        foto='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVqLNqGPby5EmndDzu_WrWt_zilZCfLnrf8g&usqp=CAU'
+        />
+        <Fotos
+        foto='https://www.proverdepaisagismo.com.br/imagens/preco-de-poda-de-plantas-de-jardim.jpg'
+        />
+         <Fotos
+        foto='https://i.pinimg.com/564x/6d/65/d9/6d65d9d200f3b68a1c837666dcc9bbdf.jpg'
+        />
+        <Fotos
+        foto='https://i.pinimg.com/564x/61/88/b3/6188b39401708303110b3538bbd69a52.jpg'
+        />
+        <Fotos
+        foto='https://i.pinimg.com/564x/ca/87/e5/ca87e5a1556dc9f7998dc901b60410f6.jpg'
+        />
+        </div>
+        
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Portfolio
